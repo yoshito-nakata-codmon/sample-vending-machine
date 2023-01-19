55cf837039f63741abae76f5db4be5c3d30bcafc
Kobori Akira <a_kobori@codmon.co.jp>
2023-01-19 17:45:11 +0900

    linterを実行

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -7,59 +7,59 @@ use PHPUnit\Framework\TestCase;
 
 class MainTest extends TestCase
 {
-  const COLA = 'cola'; // コーラ120円
-  const COFFEE = 'coffee'; // コーヒー150円
-  const ENERGY_DRINK = 'energy_drink'; // エナジードリンク210円
-  
-  const NO_CHANGE = 'nochange'; // おつりなし
+    public const COLA = 'cola'; // コーラ120円
+    public const COFFEE = 'coffee'; // コーヒー150円
+    public const ENERGY_DRINK = 'energy_drink'; // エナジードリンク210円
 
-  // 120円支払ってコーラを買う
-  const INPUT_A = ['coins' => [
-    '100' => 1,
-    '10' => 2,
-  ],
-  'menu' => MainTest::COLA];
+    public const NO_CHANGE = 'nochange'; // おつりなし
 
-  // 200円支払ってコーヒーを買う
-  const INPUT_B = ['coins' => [
-    '100' => 2,
-  ],
-  'menu' => MainTest::COFFEE];
+    // 120円支払ってコーラを買う
+    public const INPUT_A = ['coins' => [
+      '100' => 1,
+      '10' => 2,
+    ],
+    'menu' => MainTest::COLA];
 
-  // 1000円支払ってエナジードリンクを買う
-  const INPUT_C = ['coins' => [
-    '1000' => 1,
-  ],
-  'menu' => MainTest::ENERGY_DRINK];
+    // 200円支払ってコーヒーを買う
+    public const INPUT_B = ['coins' => [
+      '100' => 2,
+    ],
+    'menu' => MainTest::COFFEE];
 
-  /**
-   * @dataProvider provider_もっとも通常のパターン
-   */
-  public function test_もっとも通常のパターン($userInput, $expectedChange)
-  {
-    // Given
-    $vendingMachineCoins = [
-      '500' => 999,
-      '100' => 999,
-      '50' => 999,
-      '10' => 999,
-    ];
-    $expectedChange = MainTest::NO_CHANGE;
+    // 1000円支払ってエナジードリンクを買う
+    public const INPUT_C = ['coins' => [
+      '1000' => 1,
+    ],
+    'menu' => MainTest::ENERGY_DRINK];
 
-    // When
-    $actual = Main::run($vendingMachineCoins, $userInput);
+    /**
+     * @dataProvider provider_もっとも通常のパターン
+     */
+    public function test_もっとも通常のパターン($userInput, $expectedChange)
+    {
+        // Given
+        $vendingMachineCoins = [
+          '500' => 999,
+          '100' => 999,
+          '50' => 999,
+          '10' => 999,
+        ];
+        $expectedChange = MainTest::NO_CHANGE;
 
-    // Then
-    $this->assertSame($actual, $expectedChange);
-  }
+        // When
+        $actual = Main::run($vendingMachineCoins, $userInput);
 
-  public function provider_もっとも通常のパターン()
-  {
-      // $userInput, $expectedChange
-      return [
-          "120円支払ってコーラを買う" => [MainTest::INPUT_A, MainTest::NO_CHANGE],
-          "200円支払ってコーヒーを買う" => [MainTest::INPUT_B, "50 1"],
-          "1000円支払ってエナジードリンクを買う" => [MainTest::INPUT_C, "500 1 100 2 50 1 10 4"],
-      ];
-  }
+        // Then
+        $this->assertSame($actual, $expectedChange);
+    }
+
+    public function provider_もっとも通常のパターン()
+    {
+        // $userInput, $expectedChange
+        return [
+            "120円支払ってコーラを買う" => [MainTest::INPUT_A, MainTest::NO_CHANGE],
+            "200円支払ってコーヒーを買う" => [MainTest::INPUT_B, "50 1"],
+            "1000円支払ってエナジードリンクを買う" => [MainTest::INPUT_C, "500 1 100 2 50 1 10 4"],
+        ];
+    }
 }
