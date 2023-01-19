9718cd17761f287af57da98a1d45e492ce1daf0b
Kobori Akira <a_kobori@codmon.co.jp>
2023-01-19 17:54:25 +0900

    整理

--- README.md
+++ README.md
@@ -8,8 +8,17 @@
 composer install
 ```
 
-`composer test`が実行できていればOK。
+## 課題
+
+### 1. `test_もっとも通常のパターン`がパスするように`src`以下を修正する。
+
+
+```shell
+# 確認コマンド
+composer test-round1
+```
 
 ## linter (php-cs-fixer)
 
 プロジェクト直下で`composer lint`を実行。`src`内が勝手にフォーマットされます。
+
--- tests/MainTest.php
+++ tests/MainTest.php
@@ -7,6 +7,14 @@ use PHPUnit\Framework\TestCase;
 
 class MainTest extends TestCase
 {
+    // ほぼ無限に硬貨が補充されている
+    public const VENDING_MACHINE_COINS_INF = [
+      '500' => 999,
+      '100' => 999,
+      '50' => 999,
+      '10' => 999,
+    ];
+
     public const COLA = 'cola'; // コーラ120円
     public const COFFEE = 'coffee'; // コーヒー150円
     public const ENERGY_DRINK = 'energy_drink'; // エナジードリンク210円
@@ -33,21 +41,14 @@ class MainTest extends TestCase
     'menu' => MainTest::ENERGY_DRINK];
 
     /**
+     * @group round1
      * @dataProvider provider_もっとも通常のパターン
      */
     public function test_もっとも通常のパターン($userInput, $expectedChange)
     {
         // Given
-        $vendingMachineCoins = [
-          '500' => 999,
-          '100' => 999,
-          '50' => 999,
-          '10' => 999,
-        ];
-        $expectedChange = MainTest::NO_CHANGE;
-
         // When
-        $actual = Main::run($vendingMachineCoins, $userInput);
+        $actual = Main::run(MainTest::VENDING_MACHINE_COINS_INF, $userInput);
 
         // Then
         $this->assertSame($actual, $expectedChange);
