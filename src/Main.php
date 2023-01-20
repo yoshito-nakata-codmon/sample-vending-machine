5c1967b8ffdb724c12bd28a5de9884cbb6aa8972
Kobori Akira <a_kobori@codmon.co.jp>
2023-01-20 12:03:55 +0900

    使わないメソッドを削除

--- src/Main.php
+++ src/Main.php
@@ -21,30 +21,4 @@ class Main
     {
         return "do implementation";
     }
-
-    /**
-     * 処理の開始地点。ただし自動販売機がいくつ硬貨を持っているかも合わせて処理する
-     *
-     * @param array $vendingMachineCoins 自販機に補充される硬貨
-     * @param array $userInput 投入額と注文。前述の$coinsと$menuをあわせたもの
-     * @return string おつり。大きな硬貨順に枚数を並べる。なしの場合はnochange
-     * ex.)
-     * - 100円3枚、50円1枚、10円3枚なら"100 3 50 1 10 3"
-     */
-    public static function run(array $vendingMachineCoins, array $userInput): string
-    {
-        return "do implementation";
-    }
-
-    /**
-     * 処理の開始地点。ただし複数の客が連続して購入する場合を処理する
-     *
-     * @param array $vendingMachineCoins 自販機に補充される硬貨
-     * @param array $userInputs (複数の)客の投入額と注文
-     * @return array それぞれに対するおつり
-     */
-    public static function runMulti(array $vendingMachineCoins, array $userInputs): array
-    {
-        return ["do implementation"];
-    }
 }
